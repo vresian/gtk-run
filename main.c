ee724157b03b38cc44e94ffb565475d7d643d4e4
vresian <vresian@proton.me>
2025-05-03 10:52:44 +0200

    I don't even know what changes I've made

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
@@ -7,34 +6,40 @@
 
 int
 main (int argc, 
-      char *args[])
+      char *argv[])
 {
-    char* cwd = getcwd(NULL, 0);
-    char buf[1024];
     bool watch = false;
+    char *build_directory_name, *directory;
+    char buf[1024];
+    
+    directory = getcwd(NULL, 0);
 
     for (int i = argc - 1; i > 0; i--) 
     {
-        if (strcmp(args[i], "--watch") == 0)
+        if (strcmp(argv[i], "--watch") == 0)
         {
             watch = true;
         }
-        else if (strcmp(args[i], "--directory") == 0)
+        else if (strcmp(argv[i], "--directory") == 0)
         {
-            free(cwd);
-            cwd = args[i + 1];
+            free(directory);
+            directory = argv[i + 1];
         }
     }
 
     int fd = inotify_init();
-    int wd = inotify_add_watch(fd, cwd, IN_MODIFY);
+    int wd = inotify_add_watch(fd, directory, IN_MODIFY);
+    
+    char *setup_command = strcat();
 
     while (true)
     {
+        chdir(directory);
+        
         ////////////// 
 
         if (!watch) {
-            free(cwd);
+            free(directory);
             break;
         }
         
